e920b113f6b6eb5fba21d1fdab6e33ed396749d4
SAGAR <sagarshukla010@gmail.com>
2020-11-24 08:33:12 +0530

    frontend designing

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express');
+const { static } = require('express');
+var express = require('express');
 
 var app  = express();
+
+//app.use('/static',express.static(__dirname + '/public/server.html'))
+
+app.get('/static',(req, res)=>{
+    res.sendFile(__dirname + '/public/server.html');
+  });
+
 
 var server = app.listen(3000,() =>{
 
